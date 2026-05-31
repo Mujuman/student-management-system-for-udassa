2325414ba97d19fe8e386ee3085605fbca7adb32
mujuman <mujahidhussen75@gmail.com>
2026-06-01 01:16:59 +0300

    adddd

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 // Rate limiting
 const limiter = rateLimit({
